9c6b587bd8552dc92977c9914bf4732b5499a574
Mert Erogu <merteroglu16@gmail.com>
2017-10-26 13:52:49 +0300

    v0.3.2
    -

--- yazlabProject2/yazlabProject2/Solver.h
+++ yazlabProject2/yazlabProject2/Solver.h
@@ -2,7 +2,13 @@
 
 void storeClueGiven(int sudoku[9][9]);
 void storePositions();
+void storePositions2();
+void storePositions3();
+void storePositions4();
 bool goBack(int &row, int &column, int sudoku[9][9]);
+bool goBack2(int &row, int &column, int sudoku[9][9]);
+bool goBack3(int &row, int &column, int sudoku[9][9]);
+bool goBack4(int &row, int &column, int sudoku[9][9]);
 bool placeNum(int row, int column, int sudoku[9][9]);
 bool solveT1(int sudoku[9][9]);
 bool solveT2(int sudoku[9][9]);
